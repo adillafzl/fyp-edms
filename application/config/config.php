81fc0cd44b5cbcaa5e36c318d187055e1f80d510
adillafzl <57579009+adillafzl@users.noreply.github.com>
2020-07-21 15:04:47 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/fyp/';
+//$config['base_url'] = 'http://localhost/fyp/';
+$config['base_url'] = 'https://fypfarah.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
